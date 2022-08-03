fc889c7d468dcf6aa3cc79aaa4c7d94b039292ec
Daniel Aschwanden <daniel.aschwanden@locatee.ch>
2022-08-04 00:45:16 +0200

    switch to hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 import "./App.scss";
 
