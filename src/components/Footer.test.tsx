4a9fc8168cdb0ea25fcaa3e820b4dc02171a6198
Daniel Aschwanden <daniel.aschwanden@locatee.ch>
2022-08-04 23:12:42 +0200

    fix tests

--- src/components/Footer.test.tsx
+++ src/components/Footer.test.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import Footer from "./Footer";
 
 test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Footer />);
+  const linkElement = screen.getByText(/ZSO Uri/i);
   expect(linkElement).toBeInTheDocument();
 });
