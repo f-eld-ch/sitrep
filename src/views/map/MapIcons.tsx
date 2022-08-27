1a089853a4e1800d8010b86a67896d3cc685934b
Daniel Aschwanden <nimdanitro@gmail.com>
2022-08-27 17:51:27 +0200

    make icons customizable

--- src/views/map/MapIcons.tsx
+++ src/views/map/MapIcons.tsx
@@ -1,40 +1,48 @@
+import MapboxDraw from '@mapbox/mapbox-gl-draw';
 import ABCDekontaminationsstelle from 'assets/babs/ABCDekontaminationsstelle.svg';
 import AbsperrungEinsatzraum from 'assets/babs/AbsperrungEinsatzraum.svg';
 import KantonalesFuehrungsorgan from 'assets/babs/KantonalesFuehrungsorgan.svg';
+import { FeatureCollection } from 'geojson';
 
 import { memo, useEffect } from "react";
 import { useMap } from "react-map-gl";
 
-const BabsIcons = [
-    {
+export const BabsIcons = {
+    ABCDekontaminationsstelle: {
         src: ABCDekontaminationsstelle,
         name: 'ABCDekontaminationsstelle'
     },
-    {
+    AbsperrungEinsatzraum: {
         src: AbsperrungEinsatzraum,
         name: 'AbsperrungEinsatzraum'
     },
-    {
+    KantonalesFuehrungsorgan: {
         src: KantonalesFuehrungsorgan,
         name: 'KantonalesFuehrungsorgan'
     }
-];
+};
 
-function MapIcons() {
+type MapIconProps = {
+    initialFeatures?: FeatureCollection;
+    draw: MapboxDraw | undefined;
+}
+
+function MapIcons(props: MapIconProps) {
 
     const map = useMap();
-
+    const { initialFeatures, draw } = props;
 
     useEffect(() => {
         map && map.current && map.current.on('load', function () {
-            BabsIcons.forEach(icon => {
-                console.log("loading icon", icon.name);
-                let customIcon = new Image(24, 24);
-                customIcon.onload = () => map && map.current && map.current.addImage(icon.name, customIcon)
+            Object.values(BabsIcons).forEach(icon => {
+                let customIcon = new Image(48, 48);
+                customIcon.onload = () => map && map.current && !map.current.hasImage(icon.name) && map.current.addImage(icon.name, customIcon)
                 customIcon.src = icon.src;
             });
+            console.log("loading initial features", initialFeatures);
+            initialFeatures && draw && draw.set(initialFeatures);
         });
-    }, [map]);
+    }, [map, initialFeatures, draw]);
 
     return null;
 }
--- src/views/map/index.tsx
+++ src/views/map/index.tsx
@@ -1,71 +1,85 @@
-import GFS from 'assets/babs/Gemeindefuehrungsorgan.svg';
-import KFO from 'assets/babs/KantonalesFuehrungsorgan.svg';
 
 import MapboxDraw from '@mapbox/mapbox-gl-draw';
 import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
-import { Feature } from 'geojson';
+import { Feature, FeatureCollection } from 'geojson';
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
-import { memo, useCallback, useMemo, useState } from 'react';
-import { FullscreenControl, Map, Marker, NavigationControl, ScaleControl } from 'react-map-gl';
+import { memo, useCallback, useMemo, useRef, useState } from 'react';
+import { FullscreenControl, Map, MapRef, Marker, NavigationControl, ScaleControl } from 'react-map-gl';
 import DrawControl from './DrawControl';
-import MapIcons from './MapIcons';
+import MapIcons, { BabsIcons } from './MapIcons';
+import style from './style';
 
 const modes = {
     ...MapboxDraw.modes,
 };
 
-export function MapComponent() {
-    const [features, setFeatures] = useState<Record<string, Feature>>({ "0976882431873bcfda97359f59130c9d": { "id": "0976882431873bcfda97359f59130c9d", "type": "Feature", "properties": {}, "geometry": { "coordinates": [[[8.607680963550678, 46.89578989058219], [8.61184215415031, 46.87566509030461], [8.617468181800191, 46.868745215725966], [8.62580765757292, 46.86330503277219], [8.634872083387009, 46.86679101862971], [8.635447501666128, 46.86819710450147], [8.635911147888134, 46.869240607253786], [8.635724862309237, 46.870022460074644], [8.644109400305268, 46.87869760610951], [8.643636645639916, 46.88026030880803], [8.643832261331404, 46.88169440033174], [8.641183516418895, 46.8828195980282], [8.637974077997344, 46.88327073806448], [8.635823690747458, 46.88529114356109], [8.625019157712586, 46.89371511074398], [8.614934990165153, 46.90042062680402], [8.613948249417774, 46.89955987746683], [8.607680963550678, 46.89578989058219]]], "type": "Polygon" } }, "a00dc8d7b2614aa9a7cef2fcf7eeaecb": { "id": "a00dc8d7b2614aa9a7cef2fcf7eeaecb", "type": "Feature", "properties": {}, "geometry": { "coordinates": [8.643350717398818, 46.86109743093846], "type": "Point" } } }
-    );
 
+export function MapComponent() {
+    const [draw, setDraw] = useState<MapboxDraw>();
+    const mapRef = useRef<MapRef>();
+
+    const [features, setFeatures] = useState<FeatureCollection>({ "type": "FeatureCollection", "features": [{ "id": "775e8f6bc028342da6049bcdf17ee089", "type": "Feature", "properties": {}, "geometry": { "coordinates": [8.644308154993183, 46.86835026495743], "type": "Point" } }, { "id": "775e8f6bc028342da6049bcdf17ee089", "type": "Feature", "properties": {}, "geometry": { "coordinates": [8.636779598438778, 46.88505318042553], "type": "Point" } }, { "id": "beee8e1c04b4d5cc4ee175b129847389", "type": "Feature", "properties": {}, "geometry": { "coordinates": [[[8.604540756827362, 46.88790384354587], [8.611739113533702, 46.89034100033123], [8.615173191962015, 46.89052152605822], [8.616493991357515, 46.89029586880457], [8.618144990602701, 46.88844544351235], [8.623194670583871, 46.87836818856752], [8.62240219094656, 46.87723963716476], [8.620420991853337, 46.87642706545742], [8.614807594422444, 46.87647220865287], [8.608005477534704, 46.877329922150466], [8.605694078592592, 46.880760639041824], [8.602920399862057, 46.885048726685255], [8.601203360647872, 46.88784708326057], [8.599948601222138, 46.8865833274003], [8.601005240738544, 46.88780194963539], [8.604540756827362, 46.88790384354587]]], "type": "Polygon" } }, { "id": "beee8e1c04b4d5cc4ee175b129847389", "type": "Feature", "properties": {}, "geometry": { "coordinates": [[[8.604540756827362, 46.88790384354587], [8.611739113533702, 46.89034100033123], [8.615173191962015, 46.89052152605822], [8.616493991357515, 46.89029586880457], [8.618144990602701, 46.88844544351235], [8.623194670583871, 46.87836818856752], [8.62240219094656, 46.87723963716476], [8.620420991853337, 46.87642706545742], [8.614807594422444, 46.87647220865287], [8.608005477534704, 46.877329922150466], [8.605694078592592, 46.880760639041824], [8.602920399862057, 46.885048726685255], [8.601203360647872, 46.88784708326057], [8.601665640436266, 46.88671873192163], [8.601005240738544, 46.88780194963539], [8.604540756827362, 46.88790384354587]]], "type": "Polygon" } }, { "id": "beee8e1c04b4d5cc4ee175b129847389", "type": "Feature", "properties": {}, "geometry": { "coordinates": [[[8.604540756827362, 46.88790384354587], [8.611739113533702, 46.89034100033123], [8.615173191962015, 46.89052152605822], [8.616493991357515, 46.89029586880457], [8.618144990602701, 46.88844544351235], [8.623194670583871, 46.87836818856752], [8.62240219094656, 46.87723963716476], [8.620420991853337, 46.87642706545742], [8.614807594422444, 46.87647220865287], [8.608005477534704, 46.877329922150466], [8.605694078592592, 46.880760639041824], [8.602920399862057, 46.885048726685255], [8.601203360647872, 46.88784708326057], [8.601665640436266, 46.88671873192163], [8.601005240738544, 46.88780194963539], [8.604540756827362, 46.88790384354587]]], "type": "Polygon" } }, { "id": "beee8e1c04b4d5cc4ee175b129847389", "type": "Feature", "properties": {}, "geometry": { "coordinates": [[[8.604540756827362, 46.88790384354587], [8.611739113533702, 46.89034100033123], [8.615173191962015, 46.89052152605822], [8.616493991357515, 46.89029586880457], [8.618144990602701, 46.88844544351235], [8.623194670583871, 46.87836818856752], [8.62240219094656, 46.87723963716476], [8.620420991853337, 46.87642706545742], [8.614807594422444, 46.87647220865287], [8.608005477534704, 46.877329922150466], [8.605694078592592, 46.880760639041824], [8.602920399862057, 46.885048726685255], [8.601203360647872, 46.88784708326057], [8.601665640436266, 46.88671873192163], [8.601848055588391, 46.887680102391215], [8.604540756827362, 46.88790384354587]]], "type": "Polygon" } }, { "id": "beee8e1c04b4d5cc4ee175b129847389", "type": "Feature", "properties": {}, "geometry": { "coordinates": [[[8.604540756827362, 46.88790384354587], [8.611739113533702, 46.89034100033123], [8.615173191962015, 46.89052152605822], [8.616493991357515, 46.89029586880457], [8.618144990602701, 46.88844544351235], [8.623194670583871, 46.87836818856752], [8.62240219094656, 46.87723963716476], [8.620420991853337, 46.87642706545742], [8.614807594422444, 46.87647220865287], [8.608005477534704, 46.877329922150466], [8.605694078592592, 46.880760639041824], [8.602920399862057, 46.885048726685255], [8.601203360647872, 46.88784708326057], [8.601617016502672, 46.88775998179182], [8.601848055588391, 46.887680102391215], [8.604540756827362, 46.88790384354587]]], "type": "Polygon" } }, { "id": "beee8e1c04b4d5cc4ee175b129847389", "type": "Feature", "properties": {}, "geometry": { "coordinates": [[[8.604540756827362, 46.88790384354587], [8.611739113533702, 46.89034100033123], [8.615173191962015, 46.89052152605822], [8.616493991357515, 46.89029586880457], [8.618144990602701, 46.88844544351235], [8.623194670583871, 46.87836818856752], [8.62240219094656, 46.87723963716476], [8.620420991853337, 46.87642706545742], [8.614807594422444, 46.87647220865287], [8.608005477534704, 46.877329922150466], [8.605694078592592, 46.880760639041824], [8.602920399862057, 46.885048726685255], [8.601203360647872, 46.88784708326057], [8.601617016502672, 46.88775998179182], [8.601848055588391, 46.887680102391215], [8.604540756827362, 46.88790384354587]]], "type": "Polygon" } }, { "id": "beee8e1c04b4d5cc4ee175b129847389", "type": "Feature", "properties": {}, "geometry": { "coordinates": [[[8.604540756827362, 46.88790384354587], [8.611739113533702, 46.89034100033123], [8.615173191962015, 46.89052152605822], [8.616493991357515, 46.89029586880457], [8.618144990602701, 46.88844544351235], [8.623194670583871, 46.87836818856752], [8.62240219094656, 46.87723963716476], [8.620420991853337, 46.87642706545742], [8.614807594422444, 46.87647220865287], [8.608005477534704, 46.877329922150466], [8.605694078592592, 46.880760639041824], [8.602920399862057, 46.885048726685255], [8.601203360647872, 46.88784708326057], [8.60145493672394, 46.88775998179182], [8.601848055588391, 46.887680102391215], [8.604540756827362, 46.88790384354587]]], "type": "Polygon" } }] }
+    );
     const onUpdate = useCallback(e => {
-        setFeatures(currFeatures => {
-            const newFeatures = { ...currFeatures };
-            for (const f of e.features) {
-                newFeatures[f.id] = f;
+        console.log(e);
+        setFeatures(curFeatureCollection => {
+            const newFeatureCollection = { ...curFeatureCollection };
+            const features: Feature[] = e.features;
+            for (const f of features) {
+                if (f.geometry.type === "Point") {
+                    f.properties = Object.assign({}, f.properties, { "icon-name": BabsIcons.KantonalesFuehrungsorgan.name });
+                    draw?.setFeatureProperty(f.id?.toString() || "", "icon-name", BabsIcons.KantonalesFuehrungsorgan.name)
+                    console.log(draw?.getAll());
+                }
+                newFeatureCollection.features.push(f);
             }
-            return newFeatures;
+            return newFeatureCollection;
         });
-    }, []);
+    }, [draw]);
 
     const onDelete = useCallback(e => {
-        setFeatures(currFeatures => {
-            const newFeatures = { ...currFeatures };
-            for (const f of e.features) {
-                delete newFeatures[f.id];
+        setFeatures(curFeatureCollection => {
+            const newFeatureCollection = { ...curFeatureCollection };
+            const features: Feature[] = e.features;
+
+            for (const f of features) {
+                newFeatureCollection.features = curFeatureCollection.features.filter((e) => e.id !== f.id);
             }
-            return newFeatures;
+            return newFeatureCollection;
         });
     }, []);
 
     const onCombine = useCallback(e => {
-        setFeatures(currFeatures => {
-            const newFeatures = { ...currFeatures };
-            for (const f of e.deletedFeatures) {
-                delete newFeatures[f.id];
+        setFeatures(curFeatureCollection => {
+            const newFeatureCollection = { ...curFeatureCollection };
+            const createdFeatures: Feature[] = e.createdFeatures;
+            const deletedFeatures: Feature[] = e.deletedFeatures;
+
+            for (const f of deletedFeatures) {
+                newFeatureCollection.features = curFeatureCollection.features.filter((e) => e.id !== f.id);
             }
-            for (const f of e.createdFeatures) {
-                newFeatures[f.id] = f;
+            for (const f of createdFeatures) {
+                newFeatureCollection.features.push(f);
             }
-            return newFeatures;
+            return newFeatureCollection;
         });
     }, []);
 
     const [symbols] = useState([{
         id: 1,
         coordinates: [46.88061, 8.64463],
-        icon: KFO,
+        icon: BabsIcons.KantonalesFuehrungsorgan.src,
     },
     {
         id: 2,
         coordinates: [46.88074, 8.61234],
-        icon: GFS,
+        icon: BabsIcons.KantonalesFuehrungsorgan.src,
     },
     {
         id: 3,
         coordinates: [46.86240, 8.62903],
-        icon: GFS,
+        icon: BabsIcons.KantonalesFuehrungsorgan.src,
     },
     ]);
 
@@ -79,10 +93,15 @@ export function MapComponent() {
         </Marker>)
     ), [symbols]);
 
+    const onMapLoad = useCallback(() => {
+        mapRef && mapRef.current && mapRef.current.on("load", () => console.log("maploaded"))
+    }, []);
+
     return (
         <>
             <h3 className="title is-size-3 is-capitalized">Lage</h3>
             <Map
+                onLoad={onMapLoad}
                 mapLib={maplibregl}
                 attributionControl={true}
                 initialViewState={{
@@ -97,7 +116,9 @@ export function MapComponent() {
                 <NavigationControl position={'top-left'} visualizePitch={true} />
                 <DrawControl
                     position="top-right"
+                    setDraw={setDraw}
                     displayControlsDefault={false}
+                    styles={style}
                     controls={{
                         polygon: true,
                         trash: true,
@@ -106,7 +127,6 @@ export function MapComponent() {
                         combine_features: false,
                         uncombine_features: false,
                     }}
-                    initialFeatures={features}
                     boxSelect={false}
                     clickBuffer={10}
                     defaultMode="draw_point"
@@ -117,11 +137,11 @@ export function MapComponent() {
                     onCombine={onCombine}
                     userProperties={true}
                 />
-                <MapIcons />
+                <MapIcons draw={draw} initialFeatures={features} />
                 <ScaleControl unit={"metric"} position={'bottom-left'} />
                 {markers}
             </Map>
-            <div>{JSON.stringify(features)}</div>
+            {/* <div>{JSON.stringify(features)}</div> */}
         </>
     );
 }
