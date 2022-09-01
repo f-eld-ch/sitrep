866c04b2d2dbd0f514779cc45156d244b5875090
Daniel Aschwanden <nimdanitro@gmail.com>
2022-09-01 13:41:10 +0200

    improve map editor

--- src/views/map/style.ts
+++ src/views/map/style.ts
@@ -135,7 +135,7 @@ export const style = [
         'paint': {
             'line-pattern': ['match', ['get', 'user_lineType'], 'unpassierbar', 'PatternLineUnpassierbar', 'beabsichtigteErkundung', 'PatternLineBeabsichtigteErkundung', 'durchgeführteErkundung', 'PatternLineErkundung', 'Rutschgebiet', 'PatternLineRutschgebiet', 'RutschgebietGespiegelt', 'PatternLineRutschgebietGespiegelt', 'PatternLineUnpassierbar'],
             'line-opacity': 0.7,
-            'line-width': ['interpolate', ['linear'], ['zoom'], 12, 1, 19, 22],
+            'line-width': ['interpolate', ['linear'], ['zoom'], 10, 2, 19, 22],
         }
     },
     {
@@ -190,7 +190,7 @@ export const style = [
         'layout': {
             'icon-image': ["get", "user_icon"],
             'icon-allow-overlap': true,
-            'icon-size': ['interpolate', ['linear'], ['zoom'], 9, 0.2, 18, 1],
+            'icon-size': ['interpolate', ['linear'], ['zoom'], 9, 0.1, 17, 1],
         }
     },
     {
@@ -206,7 +206,7 @@ export const style = [
         ],
         'layout': {
             'icon-image': ["get", "user_icon"],
-            'icon-size': ['interpolate', ['linear'], ['zoom'], 9, 0.1, 18, 1],
+            'icon-size': ['interpolate', ['linear'], ['zoom'], 9, 0.1, 17, 1],
             'icon-allow-overlap': true,
             'icon-rotation-alignment': 'map',
             'icon-pitch-alignment': 'map',
@@ -269,7 +269,7 @@ export const style = [
             'icon-image': ['coalesce', ["get", "user_icon"], 'default_marker'],
             'icon-pitch-alignment': 'viewport',
             'icon-allow-overlap': true,
-            'icon-size': ['interpolate', ['linear'], ['zoom'], 9, 0.1, 18, 1],
+            'icon-size': ['interpolate', ['linear'], ['zoom'], 9, 0.1, 17, 1],
         }
     },
     {
@@ -284,7 +284,7 @@ export const style = [
         'layout': {
             'icon-image': ['coalesce', ["get", "user_icon"], 'default_marker'],
             'icon-allow-overlap': true,
-            'icon-size': ['interpolate', ['linear'], ['zoom'], 9, 0.1, 18, 1],
+            'icon-size': ['interpolate', ['linear'], ['zoom'], 9, 0.1, 17, 1],
             'icon-rotation-alignment': 'map',
             'icon-pitch-alignment': 'map',
             'icon-rotate': ['coalesce', ["get", "user_iconRotation"], 0]
@@ -322,12 +322,13 @@ export const style = [
         'layout': {
             'text-field': ["coalesce", ["get", "user_name"], ""],
             'text-font': ["Frutiger Neue Condensed Bold"],
-            'text-size': ['interpolate', ['linear'], ['zoom'], 9, 4, 18, 24],
+            'text-size': ['interpolate', ['exponential', 2], ['zoom'], 9, 4, 18, 24],
+            'symbol-placement': 'line',
+            'text-ignore-placement': true,
             'text-anchor': 'center'
         },
         'paint': {
             'text-color': ['coalesce', ['get', 'user_color'], '#000000'],
-            'text-halo-blur': 25,
             'text-halo-color': '#fff'
         }
     },
