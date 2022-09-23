df2601bb87b9d7cdb93f12d4cf507eec19b98238
Daniel Aschwanden <daniel.aschwanden@locatee.ch>
2022-09-23 09:42:57 +0200

    MessageSheet respects journal filter states

--- src/views/journal/Table.tsx
+++ src/views/journal/Table.tsx
@@ -4,12 +4,19 @@ import { useTranslation } from "react-i18next";
 import ReactMarkdown from "react-markdown";
 import { Message } from "types";
 
-function MessageTable(props: { messages: undefined | Message[] }) {
+function MessageTable(props: { messages: undefined | Message[], assignmentFilter: string, triageFilter: string, priorityFilter: string }) {
   const { t } = useTranslation();
+  const { assignmentFilter, priorityFilter, triageFilter } = props;
 
   return (
     <div className="is-clearfix">
+      <h3 className="title is-3">{t('journal')}
+        {assignmentFilter === "all" && triageFilter === "all" && priorityFilter === "all" ? <></> :
+          " (gefiltert)"}
+      </h3>
+
       <h5 className="subtitle is-7">{t('state')}: {dayjs(Date.now()).format("LLL")}</h5>
+      <FilterState assignmentFilter={assignmentFilter} priorityFilter={priorityFilter} triageFilter={triageFilter} />
       <table className="table is-fullwidth is-narrow">
         <thead>
           <tr>
@@ -39,6 +46,24 @@ function MessageTable(props: { messages: undefined | Message[] }) {
       </table>
     </div>
   );
+}
+
+function FilterState(props: { assignmentFilter: string, triageFilter: string, priorityFilter: string }) {
+
+  const { assignmentFilter, priorityFilter, triageFilter } = props;
+  const { t } = useTranslation();
+
+  if (assignmentFilter === "all" && triageFilter === "all" && priorityFilter === "all") {
+    return null;
+  }
+
+  return (
+    <h5 className="subtitle is-7"><b>Filter: </b>
+      {assignmentFilter !== "all" && <p>{t('divisions')}: {assignmentFilter}</p>}
+      {priorityFilter !== "all" && <p>{t('message.priority')}: {t(`priority.${priorityFilter}`)}</p>}
+      {triageFilter !== "all" && <p>{t('message.triage')}: {t(`triage.${triageFilter}`)}</p>}
+    </h5>
+  )
 }
 
 export default memo(MessageTable);
