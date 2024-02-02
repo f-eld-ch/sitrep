ede361204ef04b550273347f7c89569f1e82b466
Daniel Aschwanden <nimdanitro@gmail.com>
2024-02-02 01:25:26 +0100

    feat(ui): change logo and footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className="footer has-background-white-bis is-hidden-print">
       <div className="content has-text-centered">
-        <p>
+        <p className="is-size-7 is-family-monospace">
           Made with <FontAwesomeIcon icon={faHeart} color="red" /> in Switzerland by <strong>F-ELD</strong>
         </p>
       </div>
