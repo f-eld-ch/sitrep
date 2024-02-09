9effb2c48f2a9fa725304e2f3422f4d9fb45f01a
Daniel Aschwanden <nimdanitro@gmail.com>
2024-02-09 13:28:01 +0100

    feat(service-worker): Add new service worker configuration

--- ui/src/vite-env.d.ts
+++ ui/src/vite-env.d.ts
@@ -1,12 +1,14 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+/// <reference types="vite-plugin-pwa/client" />
+/// <reference types="vite-plugin-pwa/react" />
 
-import { FC, SVGProps } from 'react';
+import { FC, SVGProps } from "react";
 
-declare module '*.png';
-declare module '*.jpeg';
-declare module '*.jpg';
-declare module '*.svg' {
-    const content: FC<SVGProps<SVGElement>>;
-    export default content;
+declare module "*.png";
+declare module "*.jpeg";
+declare module "*.jpg";
+declare module "*.svg" {
+  const content: FC<SVGProps<SVGElement>>;
+  export default content;
 }
