11e25e6cb9db6f011786b74425c0763e8f82775d
Daniel Aschwanden <nimdanitro@gmail.com>
2024-05-14 10:06:12 +0200

    feat(ui): initial version of DarkMode

--- ui/src/components/Footer.tsx
+++ ui/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Footer() {
   return (
-    <footer className="footer has-background-white-bis is-hidden-print">
+    <footer className="footer is-hidden-print">
       <div className="content has-text-centered">
         <p className="is-size-7 is-family-monospace">
           Made with <FontAwesomeIcon icon={faHeart} color="red" /> in Switzerland by <strong>F-ELD</strong>
